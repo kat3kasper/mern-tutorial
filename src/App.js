8f40013eb9a43927c73ebc619da82a895359c985
Katelyn Kasperowicz <kat3kasper@gmail.com>
2017-06-09 12:51:10 -0400

    3.3

--- src/App.js
+++ src/App.js
@@ -14,12 +14,38 @@ class BugFilter extends React.Component {
 }
 class BugTable extends React.Component {
   render() {
-    return <h1>BugTable</h1>;
+    return <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Status</th>
+          <th>Priority</th>
+          <th>Owner</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <BugRow id={1} status="Open" priority="High" owner="Marco" title="bug numero uno"/>
+        <BugRow id={2} status="In Progress" priority="Low" owner="Anne Marie" title="number 2"/>
+      </tbody>
+    </table>;
   }
 }
 class BugAdd extends React.Component {
   render() {
     return <h1>BugAdd</h1>;
+  }
+}
+
+class BugRow extends React.Component {
+  render() {
+    return <tr>
+      <td>{this.props.id}</td>
+      <td>{this.props.status}</td>
+      <td>{this.props.priority}</td>
+      <td>{this.props.owner}</td>
+      <td>{this.props.title}</td>
+    </tr>;
   }
 }
 ReactDOM.render(
--- static/App.js
+++ static/App.js
@@ -1,7 +1,7 @@
 class BugList extends React.Component {
   render() {
     return React.createElement(
-      'div',
+      "div",
       null,
       React.createElement(BugFilter, null),
       React.createElement(BugTable, null),
@@ -12,27 +12,99 @@ class BugList extends React.Component {
 class BugFilter extends React.Component {
   render() {
     return React.createElement(
-      'h1',
+      "h1",
       null,
-      'BugFilter'
+      "BugFilter"
     );
   }
 }
 class BugTable extends React.Component {
   render() {
     return React.createElement(
-      'h1',
+      "table",
       null,
-      'BugTable'
+      React.createElement(
+        "thead",
+        null,
+        React.createElement(
+          "tr",
+          null,
+          React.createElement(
+            "th",
+            null,
+            "ID"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Status"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Priority"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Owner"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Title"
+          )
+        )
+      ),
+      React.createElement(
+        "tbody",
+        null,
+        React.createElement(BugRow, { id: 1, status: "Open", priority: "High", owner: "Marco", title: "bug numero uno" }),
+        React.createElement(BugRow, { id: 2, status: "In Progress", priority: "Low", owner: "Anne Marie", title: "number 2" })
+      )
     );
   }
 }
 class BugAdd extends React.Component {
   render() {
     return React.createElement(
-      'h1',
+      "h1",
       null,
-      'BugAdd'
+      "BugAdd"
+    );
+  }
+}
+
+class BugRow extends React.Component {
+  render() {
+    return React.createElement(
+      "tr",
+      null,
+      React.createElement(
+        "td",
+        null,
+        this.props.id
+      ),
+      React.createElement(
+        "td",
+        null,
+        this.props.status
+      ),
+      React.createElement(
+        "td",
+        null,
+        this.props.priority
+      ),
+      React.createElement(
+        "td",
+        null,
+        this.props.owner
+      ),
+      React.createElement(
+        "td",
+        null,
+        this.props.title
+      )
     );
   }
 }
