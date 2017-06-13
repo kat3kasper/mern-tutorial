56a390e7df95e3df1c71cc0c685956c13eb54c3b
Katelyn Kasperowicz <kat3kasper@gmail.com>
2017-06-12 23:13:31 -0400

    6.3

--- webapp.js
+++ webapp.js
@@ -8,9 +8,6 @@ var db;
 app.use(bodyParser.json()); // for parsing application/json
 app.use(express.static('static'));
 
-let bugs = [{id: 1, status: "Open", priority: "High", owner: "Marco", title: "Bug numero uno"},
-							{id: 2, status: "In Progress", priority: "Low", owner: "Anne Marie", title: "Numba 2"}];
-
 app.get('/api/bugs', function(req, res) {
 	db.collection("bugs").find().toArray(function(err, docs){
 		res.status(200).send(JSON.stringify(docs));
@@ -20,9 +17,12 @@ app.get('/api/bugs', function(req, res) {
 app.post('/api/bugs', function(req, res) {
 	console.log("Request body: ", req.body);
 	let newBug = req.body;
-	newBug.id = bugs.length + 1;
-	bugs.push(newBug);
-	res.json(newBug);
+	db.collection("bugs").insertOne(newBug).then((r) => {
+		const id = r.insertedId;
+		db.collection("bugs").find({_id: id}).limit(1).next(function(err, docs) {
+			res.json(docs);
+		})
+	})
 })
 
 MongoClient.connect('mongodb://localhost/mern', function(err, dbConnection) {
