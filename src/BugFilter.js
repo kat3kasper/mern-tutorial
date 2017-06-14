87edc9e198c76aa6ac72a8b9dd2e52d7add9fef6
Katelyn Kasperowicz <kat3kasper@gmail.com>
2017-06-14 15:20:22 -0400

    8.3

--- src/BugFilter.js
+++ src/BugFilter.js
@@ -3,14 +3,50 @@ var React = require('react');
 export default class BugFilter extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {status: '', priority: ''};
+
     this.submit = this.submit.bind(this);
+    this.handleStatus = this.handleStatus.bind(this);
+    this.handlePriority = this.handlePriority.bind(this);
   }
 
-  submit() {
-    this.props.onSubmit({priority: 'High'});
+  handleStatus(event) {
+    this.setState({status: event.target.value});
   }
+
+  handlePriority(event) {
+    this.setState({priority: event.target.value});
+  }
+
+  submit(event) {
+    event.preventDefault();
+    this.props.onSubmit({status: this.state.status, priority: this.state.priority});
+  }
+
   render() {
     console.log('BugFilter rendered');
-    return <button onClick={this.submit}>Filter</button>;
+    return (
+      <form onSubmit={this.submit}>
+        <label>
+          Status:
+          <select value={this.state.status} onChange={this.handleStatus}>
+            <option value="">Any</option>
+            <option value="Open">Open</option>
+            <option value="In-Progress">In-Progress</option>
+            <option value="Closed">Closed</option>
+          </select>
+        </label>
+        <label>
+          Priority:
+          <select value={this.state.priority} onChange={this.handlePriority}>
+            <option value="">Any</option>
+            <option value="High">High</option>
+            <option value="Medium">Medium</option>
+            <option value="Low">Low</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+    )
   }
 }
